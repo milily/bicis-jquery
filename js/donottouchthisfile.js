bdda7ce4fe10e0924b8bf15ec3ce2045ebc754d7
mily rivas <mrise.translations@gmail.com>
2017-07-10 17:43:23 -0400

    validacion de nombre y apellido listo

--- js/donottouchthisfile.js
+++ js/donottouchthisfile.js
@@ -1,5 +1,6 @@
-var element = document.querySelector(".form-signup");
+/*var element = document.querySelector(".form-signup");
 element.addEventListener("submit", function(event) {
   event.preventDefault();
   validateForm();
 });
+*/
--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-function validateForm(){
-	/* Escribe tú código aquí */
-}
+$(document).ready(function () {
+    var nombre = /[0-9]/;
+	var apellido = /[0-9]/;
+	var valEmail = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+    $(".submit-btn").click(function (){
+        if( $("#name").val() == "" || nombre.test($("#name").val()) || $("#name").val().charAt(0) != $("#name").val().charAt(0).toUpperCase()){
+            $("#name").focus().after("<span class='error'>Ingrese su nombre/primera mayúscula/sin números</span>");
+            return false;
+        }else if( $("#lastname").val() == "" || apellido.test($("#lastname").val()) || $("#lastname").val().charAt(0) != $("#lastname").val().charAt(0).toUpperCase()){
+            $("#lastname").focus().after("<span class='error'>Ingrese su apellido</span>");
+            return false;
+		}else if( $("#input.email").val() == "" || !valEmail.test($("#input-email").val()) ){
+            $("#input-email").focus().after("<span class='error'>Ingrese un email correcto</span>");
+            return false;
+        }else if( $("#input-password").val() == ""){
+            $("#input-password").focus().after("<span class='error'>Ingrese una contraseña válida</span>");
+            return false;
+        }else if( $(".form-control").val() == "" ){
+            $(".form-control").focus().after("<span class='error'>Seleccione una bici</span>");
+            return false;
+        }
+    });
+    
+});
